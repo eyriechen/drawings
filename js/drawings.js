5053ad3d4d017ba95d0befae51e062908395dee3
Eyrie Chen <eyriechen@gmail.com>
2016-10-25 23:03:02 +0800

    add WireRect

--- js/drawings.js
+++ js/drawings.js
@@ -16,14 +16,31 @@ function roundedRectAnticlockwise(x, y, width, height, radius) {
        + "Q" + x + " " + (y + height) + " " + (x + radius) + " " + (y + height)
        + "L" + (x + width - radius) + " " + (y + height)
        + "Q" + (x + width) + " " + (y + height) + " " + (x + width) + " " + (y + height - radius)
-       + "L" + (x + width) + " " + (y + radius) + " " + (x + width - radius) + " " + y
+       + "L" + (x + width) + " " + (y + radius)
        + "Q" + (x + width) + " " + y + " " + (x + width - radius) + " " + y
        + "L" + (x + radius) + " " + y
        + "Q" + x + " " + y + " " + x + " " + (y + radius);
 };
 
-function WireRect(parent, x, y, width, height, diameter, r) {
+function WireRect(parent, x, y, width, height, diameter, radius) {
   this.parent = parent;
+  var radius_inside = radius - diameter;
+  if (radius_inside < 0) { radius_inside = 0; };
+  var path = roundedRectClockwise(x, y, width, height, radius) +
+             roundedRectAnticlockwise(x + radius, y + radius, width - 2 * radius, height - 2 * radius, radius_inside) +
+             "Z";
+  this.attrs = {"x": x, "y": y, "d": path};
+  this.styles = {"fill": "white", "stroke": "black"};
+};
+
+WireRect.prototype.draw = function() {
+  var w = this.parent.append("path");
+  for(var key in this.attrs){
+    w.attr(key, this.attrs[key]);
+  }
+  for (var key in this.styles) {
+    w.style(key, this.styles[key]);
+  }
 };
 
 function Wire(parent, x, y, length, diameter, direction){
